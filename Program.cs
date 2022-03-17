b3652044d94b5f023f51ef82cb2c1a81f0e9c3dc
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-17 13:36:57 +0530

    Welcome msg

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Address_book
+{
+    class Contact
+    {      
+        public void display()
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact();
+            contact.display();
+        }
+    }
+}
+    
+
+
+
+
+
+
